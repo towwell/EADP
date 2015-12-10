8d16bab0272952b230215d4c0bf1c7641ffe8f82
Irfan Iskandar <towwell@gmail.com>
2015-12-10 10:33:53 +0800

    Create default workspace

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EADP.Startup))]
+namespace EADP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
